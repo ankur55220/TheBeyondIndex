f46f711cbf65e6655fdb3870e33a8bfe7741d707
Ankur4591 <ankur.verma@repolite,com>
2025-06-26 15:20:27 +0530

    meta data update

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'The beyond index';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'The Beyond Index';
+export const SITE_DESCRIPTION = 'Exploring the cosmos, unexplained mysteries, cryptids, and other fascinating phenomena that challenge our understanding of the universe.';
